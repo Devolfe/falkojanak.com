021263ef2471ae316d92d57151c7766922d01f9a
Devolfe <g.dienwiebel@gmx.de>
2010-10-20 16:59:19 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+		<div id="footer" class="ui-widget-content ui-corner-top">
+			<ul style="margin-left: 30px; margin-right:30px; padding: 5px 0px 5px 30px;" class="ui-state-active ui-corner-all">
+				<li><a href="impressum.htm" style="text-decoration:underline; padding: 5px 10px">Impressum</a></li>
+				<li > | </li>
+				<li >"Nur tote Fische schwimmen mit dem Strom."</li>
+			</ul>
+		</div>
+	</div>
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+		try {
+		var pageTracker = _gat._getTracker("UA-7333478-4");
+		pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Falko Janak ... a developers profile</title>
+		<link href="<?php bloginfo('template_url'); ?>/ui/layout_1/layout_1.css" rel="stylesheet" type="text/css"/>
+		<link href="<?php bloginfo('template_url'); ?>/ui/jquery-design/css/pepper-grinder/jquery-ui-1.7.2.custom.css" rel="stylesheet" type="text/css"/>
+		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/ui/jquery-design/js/jquery-1.3.2.min.js"></script> 
+		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/ui/jquery-design/js/jquery-ui-1.7.2.custom.min.js"></script> 
+<script type="text/javascript">
+	$(document).ready(function(){	
+		if(navigator.appName == "Microsoft Internet Explorer"){
+			$('#navi').tabs();
+		}else{
+			$('#navi').tabs({fx: { height: 'blind', opacity: 'toggle', duration: 'normal' }});
+		}
+		$('#principles').hide();
+		$('#references').hide();
+	});
+</script>
+	
+		
+</head>
+<body>
+	<div id="root" class="ui-widget">
+		<img src="<?php bloginfo('template_url'); ?>/ui/img/logo.png" alt="Falko Janak ... a developer's profile" />
