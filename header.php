faa2c0a07ebd6c747a74788a2a15a803f0b4410c
Devolfe <g.dienwiebel@gmx.de>
2010-11-22 12:26:37 +0100

    Transparentes Logo eingefügt evtl noch Randanpassung links ändern

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <body>
 	<div id="border">
 		<div id="head">
-			<div class="storyImage"><a href="http://localhost/fj/"><img src="<?php bloginfo('template_url'); ?>/img/falkojanak_logo.png" alt="Falko Janak ... a developer's profile" /></a></div>
+			<div class="storyImage"><a href="http://localhost/fj/"><img src="<?php bloginfo('template_url'); ?>/img/falkojanak_logo_trans.png" alt="Falko Janak ... a developer's profile" /></a></div>
 	
 	
 			
